fcaf329c20f75842db5d36ebd030fad464ad050e
MaxwellErnst <109315049+MaxwellErnst@users.noreply.github.com>
2026-06-11 21:36:35 +0200

    Update config_flow.py

--- custom_components/ge_eltariff/config_flow.py
+++ custom_components/ge_eltariff/config_flow.py
@@ -4,7 +4,30 @@ from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 
+from .const import DOMAINfrom __future__ import annotations
+
+from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResult
+import voluptuous as vol
+
 from .const import DOMAIN
+
+
+class GETariffConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None) -> FlowResult:
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Göteborg Energi El-tariff",
+                data={}
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({})
+        )
+
 
 
 class GETariffConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
