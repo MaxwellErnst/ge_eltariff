98b1e2be9b3292cf6609e0c84caf557a268ab3c9
MaxwellErnst <109315049+MaxwellErnst@users.noreply.github.com>
2026-06-11 20:36:20 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,16 @@
+DOMAIN = "ge_eltariff"
+
+CONF_TARIFF = "tariff"
+CONF_POWER_SENSOR = "power_sensor"
+CONF_INCLUDE_VAT = "include_vat"
+
+DEFAULT_INCLUDE_VAT = True
+
+TARIFFS = {
+    "GN6KW": "Tidsindelad 6 kW",
+    "GN14KW": "Tidsindelad 14 kW",
+    "GN43KW": "Tidsindelad 43 kW",
+    "GNM63": "Småhus och företag max 63A",
+    "GNO63": "Normaltariff över 63A",
+    "HSP10KV": "Högspänning 10 kV",
+}
