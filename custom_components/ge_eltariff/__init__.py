787db4ff6e45aa13ef2aebc81486faac3a83f3f2
MaxwellErnst <109315049+MaxwellErnst@users.noreply.github.com>
2026-06-11 21:12:43 +0200

    Update __init__.py

--- custom_components/ge_eltariff/__init__.py
+++ custom_components/ge_eltariff/__init__.py
@@ -1,7 +1,17 @@
-from .const import DOMAIN
+from __future__ import annotations
 
-async def async_setup(hass, config):
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, PLATFORMS
+
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return unload_ok
